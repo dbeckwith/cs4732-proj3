943687a8be7967de38ee9319481e1724ae680943
Daniel Beckwith <djbsnx@gmail.com>
2017-04-14 15:51:11 -0400

    Adds team interactions.

--- proj3/boids.py
+++ proj3/boids.py
@@ -12,19 +12,31 @@ from . import util
 class BoidSim(object):
     # TODO: teams
     # TODO: obstacles
+    team_attractions = {
+        'red': 'blue',
+        'blue': 'green',
+        'green': 'red'
+    }
+    team_evasions = {
+        'red': 'green',
+        'blue': 'red',
+        'green': 'blue'
+    }
 
     def __init__(self, scene, bounds_radius):
         self.scene = scene
         self.bounds_radius = bounds_radius
         self.boids = set()
-        for _ in range(100):
-            pr = random() * bounds_radius
-            pt = random() * math.pi * 2
-            vr = Boid.speed
-            vt = random() * math.pi * 2
-            self.boids.add(Boid(self,
-                QPointF(pr * math.cos(pt), pr * math.sin(pt)),
-                QPointF(vr * math.cos(vt), vr * math.sin(vt))))
+        for team in Boid.team_colors.keys():
+            for _ in range(30):
+                pr = random() * bounds_radius
+                pt = random() * math.pi * 2
+                vr = Boid.speed
+                vt = random() * math.pi * 2
+                self.boids.add(Boid(self,
+                    team,
+                    QPointF(pr * math.cos(pt), pr * math.sin(pt)),
+                    QPointF(vr * math.cos(vt), vr * math.sin(vt))))
 
     def update(self, dt):
         for boid in self.boids:
@@ -33,16 +45,23 @@ class BoidSim(object):
             bounds_repel = util.lerp(util.length(boid.position), self.bounds_radius * 0.9, self.bounds_radius, 0, 2000, True) * -util.normalized_or_null(boid.position)
             boid.acceleration += bounds_repel
 
-            boid.neighborhood = set(neighbor for neighbor in self.boids if neighbor is not boid and util.lengthsq(neighbor.position - boid.position) < boid.visual_range * boid.visual_range)
+            boid.neighborhood = set(neighbor for neighbor in self.boids if \
+                neighbor is not boid and \
+                neighbor.team == boid.team and \
+                util.lengthsq(neighbor.position - boid.position) < boid.visual_range * boid.visual_range)
             if boid.neighborhood:
-                separation = sum((boid.position - neighbor.position for neighbor in boid.neighborhood), QPointF())
-                boid.acceleration += separation / 40 * 60
+                separation = sum((boid.position - neighbor.position for neighbor in boid.neighborhood if \
+                    boid.team == neighbor.team or \
+                    self.team_evasions[boid.team] == neighbor.team), QPointF())
+                boid.acceleration += separation / 30 * 60
 
                 avg_heading = sum((neighbor.velocity for neighbor in boid.neighborhood), QPointF()) / len(boid.neighborhood)
                 alignment = avg_heading - boid.velocity
                 boid.acceleration += alignment / 8 * 60
 
-                avg_position = sum((neighbor.position for neighbor in boid.neighborhood), QPointF()) / len(boid.neighborhood)
+                avg_position = sum((neighbor.position for neighbor in boid.neighborhood if \
+                    boid.team == neighbor.team or \
+                    self.team_attractions[boid.team] == neighbor.team), QPointF()) / len(boid.neighborhood)
                 cohesion = avg_position - boid.position
                 boid.acceleration += cohesion / 100 * 60
 
@@ -52,9 +71,16 @@ class BoidSim(object):
 class Boid(object):
     speed = 100
     visual_range = 50
+    team_colors = {
+        'red': 0,
+        'green': 120,
+        'blue': 240
+    }
 
-    def __init__(self, sim, position, velocity):
+    def __init__(self, sim, team, position, velocity):
         self.sim = sim
+
+        self.team = team
 
         self.position = position
         self.velocity = velocity
@@ -66,7 +92,7 @@ class Boid(object):
                 QPointF(-0.3, 0),
                 QPointF(-0.6, -0.6)]),
             QPen(Qt.NoPen),
-            QBrush(util.hsl(0, 90, 80)))
+            QBrush(util.hsl(self.team_colors[team], 90, 80)))
         self.body_graphic.setScale(10)
 
         self.visual_range_graphic = sim.scene.addEllipse(
