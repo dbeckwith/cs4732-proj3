76bdfb076191dc3600b381fc286827f89e245264
Daniel Beckwith <djbsnx@gmail.com>
2017-04-13 21:01:23 -0400

    Copies animation skeleton from project 2.

--- proj3/__main__.py
+++ proj3/__main__.py
@@ -1,7 +1,54 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import math
+import itertools
+
+from PyQt5.QtGui import QVector3D
+from PyQt5.QtWidgets import QApplication
+
+from .animation import Animation
+from . import util
+
+
+class Proj3Ani(Animation):
+    """
+    Implements the kinematics animation.
+    """
+
+    def __init__(self):
+        super().__init__(
+            title='CS 4732 Project 3 by Daniel Beckwith',
+            frame_rate=60.0,
+            run_time=60.0)
+
+        self.setup_scene(
+            background_color=util.hsl(0, 0, 100))
+
+    def make_scene(self):
+        """
+        Overriddes Animation.make_scene
+        """
+        pass
+
+    def update(self, frame, t, dt):
+        """
+        Overriddes Animation.update
+        """
+        pass
+
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser('proj3')
+    parser = argparse.ArgumentParser(
+        prog='proj3',
+        description='Boids simulation',
+        epilog='Created by Daniel Beckwith for WPI CS 4732.')
     args = parser.parse_args()
+
+    app = QApplication([])
+
+    ani = Proj3Ani()
+    ani.run()
+
+    sys.exit(app.exec_())
---
+++ proj3/animation.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QBrush
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
+
+from . import util
+
+
+class Animation(object):
+    """
+    Abstract class for setting up a 2D scene and animating it.
+    """
+
+    def __init__(self, title, frame_rate, run_time):
+        """
+        Create a new Animation.
+
+        Arguments:
+            title: str, the window title
+            frame_rate: float, the number of frames to display per second
+            run_time: float, the number of seconds to run the animation
+        """
+        self.title = title
+        assert 0 < frame_rate < 1000
+        self.frame_rate = frame_rate
+        assert run_time > 0
+        self.run_time = run_time
+
+        self.frame = 0
+        self.prev_update_time = None
+
+        self.scene = QGraphicsScene()
+
+        # create the window
+        self.view = QGraphicsView(self.scene)
+
+        self.view.setWindowTitle(self.title)
+
+    def setup_scene(self, background_color):
+        """
+        Sets up the scene. Should be called before running the animation.
+
+        Arguments:
+            background_color: QColor, background color of the scene
+        """
+        # set background color
+        self.view.setBackgroundBrush(QBrush(background_color))
+
+        # let subclass populate scene
+        self.make_scene()
+
+    def make_scene(self):
+        """
+        Abstract method. Populates the scene with objects.
+        """
+        raise NotImplementedError()
+
+    def _update(self):
+        """
+        Updates the animation, rendering the next frame.
+        """
+        # current animation time in seconds
+        t = util.lerp(self.frame, 0, self.frame_rate, 0, 1)
+        # change in time since the last frame
+        dt = 1 / self.frame_rate if self.prev_update_time is None else t - self.prev_update_time
+        # call subclass's frame update
+        self.update(self.frame, t, dt)
+
+        # stop the animation and close the window if run past run_time
+        if t >= self.run_time:
+            self.animation_timer.stop()
+            self.view.close()
+
+        self.prev_update_time = t
+        self.frame += 1
+
+    def update(self, frame, t, dt):
+        """
+        Abstract method. Updates one frame of the animation.
+
+        Arguments:
+            frame: int, the current frame number
+            t: float, the current animation time in seconds
+            dt: float, the number of seconds since the last update
+        """
+        raise NotImplementedError()
+
+    def run(self):
+        """
+        Runs the animation asynchronously. The animation runs in the background for self.run_time seconds.
+        """
+        self.animation_timer = QTimer(self.view)
+        # timer interval in msecs
+        self.animation_timer.setInterval(1000 / self.frame_rate)
+        # call update on each timeout of the timer
+        self.animation_timer.timeout.connect(self._update)
+        self.animation_timer.start()
+
+        # show the main window
+        self.view.show()
